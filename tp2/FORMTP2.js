7cb8456c0be204308369109669682aee6b35f84e
aitbouziad <72694395+aitbouziad@users.noreply.github.com>
2020-11-02 10:20:27 +0100

    TP2 MODIFICATION
    Modifié

---
+++ tp2/FORMTP2.js
@@ -0,0 +1,20 @@
+function validation(){
+  
+    var city = document.getElementById("city").value;
+    
+    if(city.length < 5){
+      // afficher une erreur
+      
+    }
+    
+    // ou faire une validation de tous les champs en une seule fois
+    let error = "";
+  for(let i of
+     document.querySelectorAll('.inputAW')){
+      console.log( i.value.length, 
+        i.getAttribute('maxlength'))
+      // afficher une erreur 
+      // si i.value.length < 5 && i.value.length > i.getAttribute('maxlength')
+  }
+    
+  }
