962304600290b20b304cf87e19cf70f49e825d5e
aitbouziad <72694395+aitbouziad@users.noreply.github.com>
2020-11-02 11:32:15 +0100

    tp2 updated
    modif

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,19 +1,10 @@
 function validation(){
   
     var nom = document.getElementById("nom").value;
+    var prénom = document.getElementById("prénom").value;
+    var date = document.getElementById("date").value;
+    var adresse= document.getElementById("adresse").value;
+    var mail = document.getElementById("mail").value;
     
-    if(nom.length < 5){
-      // afficher une erreur
-      
-    }
-    
-    // ou faire une validation de tous les champs en une seule fois
-    let error = "";
-  for(let i of
-     document.querySelectorAll('.inputAW')){
-      console.log( i.value.length, i.getAttribute('maxlength'))
-      // afficher une erreur 
-      // si i.value.length < 5 && i.value.length > i.getAttribute('maxlength')
-  }
-    
-  }
+   
+  
