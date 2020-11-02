22c11e18ef879224d6f15f16e2f5cfb1cda1ae71
aitbouziad <72694395+aitbouziad@users.noreply.github.com>
2020-11-02 15:33:14 +0100

    tp2 updated
    modif

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -5,6 +5,13 @@ function validation(){
     var date = document.getElementById("date").value;
     var adresse= document.getElementById("adresse").value;
     var mail = document.getElementById("mail").value;
-    
-   
   
+    let error = "";
+    for(let i of document.querySelectorAll('.inputAW')){
+        console.log( i.value.length, i.getAttribute('maxlength'))
+        
+        if{ i.value.length < 5 && i.value.length > i.getAttribute('maxlength')
+
+        document.getElementById("error").innerHTML="Veuillez renseigner tous les champs , ils doivent contenir au minimum 5 caractéres";}
+        else {document.getElementById("resultat").innerHTML="Bienvenue";
+      }
