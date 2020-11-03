f18aff408e7c3ea55f122c38cd2027e3547201b8
aitbouziad <72694395+aitbouziad@users.noreply.github.com>
2020-11-03 10:29:41 +0100

    TP2 uPDATED
    modif

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -4,7 +4,7 @@ function validation(){
   var prenom = document.getElementById("prenom").value;
   var date = document.getElementById("date").value;
   var adresse = document.getElementById("adresse").value;
-  var mail = document.getElementById("mail").value;
+  var email = document.getElementById("email").value;
  
    
   
@@ -31,5 +31,40 @@ for(let item of document.querySelectorAll('.inputAW')){
   }
    
 
+
+  Using regular expressions is probably the best way. You can see a bunch of tests here (taken from chromium)
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+Here's the example of regular expresion that accepts unicode:
+
+const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+But keep in mind that one should not rely only upon JavaScript validation. JavaScript can easily be disabled. This should be validated on the server side as well.
+
+Here's an example of the above in action:
+
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validate() {
+  const $result = $("#result");
+  const email = $("#email").val();
+  $result.text("");
+
+  if (validateEmail(email)) {
+    $result.text(email + " is valid :)");
+    $result.css("color", "green");
+  } else {
+    $result.text(email + " is not valid :(");
+    $result.css("color", "red");
+  }
+  return false;
+}
+
+$("#validate").on("click", validate);
   
 }
