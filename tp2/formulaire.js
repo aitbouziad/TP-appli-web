458842d5b639f0362a4ee05ea48921ae40c26672
aitbouziad <72694395+aitbouziad@users.noreply.github.com>
2020-11-03 16:58:12 +0100

    TP2 Updated
    modifié

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,10 +1,5 @@
 function validation(){
-  
-  var nom = document.getElementById("nom").value;
-  var prenom = document.getElementById("prenom").value;
-  var date = document.getElementById("date").value;
-  var adresse = document.getElementById("adresse").value;
-  var email = document.getElementById("email").value;
+
  
    
   
@@ -19,9 +14,14 @@ for(let item of document.querySelectorAll('.inputAW')){
     document.getElementById("error").classList.add("display");
       document.getElementById("resultat").classList.remove("display")
 
-  }
-    
+  } else if (item.value == "") {
+  var label =   document.querySelector("label[for="+item.id+"]").textContent;
+  document.getElementById("error").innerHTML += ` ce champ est obligatoire pour : ${label} <br/>`;
+  document.getElementById("error").classList.add("display");
+  document.getElementById("resultat").classList.remove("display")
 }
+}
+
   if(document.getElementById("error").innerHTML == ""){
     var prenom = document.getElementById("prenom").value
     document.getElementById("resultat").innerHTML = `Bienvenue à ${prenom}`;
@@ -32,39 +32,6 @@ for(let item of document.querySelectorAll('.inputAW')){
    
 
 
-  Using regular expressions is probably the best way. You can see a bunch of tests here (taken from chromium)
-
-function validateEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-Here's the example of regular expresion that accepts unicode:
-
-const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-But keep in mind that one should not rely only upon JavaScript validation. JavaScript can easily be disabled. This should be validated on the server side as well.
-
-Here's an example of the above in action:
-
-function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
-
-function validate() {
-  const $result = $("#result");
-  const email = $("#email").val();
-  $result.text("");
-
-  if (validateEmail(email)) {
-    $result.text(email + " is valid :)");
-    $result.css("color", "green");
-  } else {
-    $result.text(email + " is not valid :(");
-    $result.css("color", "red");
-  }
-  return false;
-}
-
-$("#validate").on("click", validate);
+  
   
 }
