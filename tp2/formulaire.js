3b84c7ae7a54214e36692b09c260150f43c7a947
aitbouziad <72694395+aitbouziad@users.noreply.github.com>
2020-11-14 23:02:53 +0100

    TP2 Updated
    Update

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,38 +1,30 @@
 function validation(){
 
-  document.getElementById("error").innerHTML = "";
+  {
+    document.getElementById("resultat").setAttribute("hidden",'hidden');//je récupure le résultat de fomulaire
+    document.getElementById("error").setAttribute("hidden",'hidden');
+//Ici on récuppère le nom et on compare sa longueur si elle est inférieure à 5 on affiche le message d'erreur 
+    if (document.getElementById("nom").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "Le nom doit contenir au moins 5 caractères !";
+         document.getElementById("error").removeAttribute("hidden");
+    }  
+     else if(document.getElementById("prenom").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "Le prenom doit contenir au moins 5 caractères !";
+         document.getElementById("error").removeAttribute("hidden");
+    }    
+      
+     else if(document.getElementById("adress").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "L'adresse doit contenir au moins 5 caractères !";
+         document.getElementById("error").removeAttribute("hidden");
+    }    
+     else if(document.getElementById("mail").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "Le mail doit contenir au moins 5 caractères !";
+         document.getElementById("error").removeAttribute("hidden");
+    }    
+     else {
+        document.getElementById("resultat").removeAttribute("hidden");
+        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#prenom").value
 
-   
-  
-for(let item of document.querySelectorAll('.inputAW')){
-
-
-
-  if (item.value == "") {
-    var label =   document.querySelector("label[for="+item.id+"]").textContent;
-    document.getElementById("error").innerHTML += ` ce champ est obligatoire pour : ${label} <br/>`;
-    document.getElementById("error").classList.add("display");
-    document.getElementById("resultat").classList.remove("display")
-  
-  } else if(item.value.length < 5){
-    // recuperation du label
-    var label =   document.querySelector("label[for="+item.id+"]").textContent;
-    // ajout du message d'erreur
-    document.getElementById("error").innerHTML += `Minimum 5 caractères pour : ${label} <br/>`;
-    
-    // ajout de la classe CSS "display"
-    document.getElementById("error").classList.add("display");
-      document.getElementById("resultat").classList.remove("display")
-
-}
-}
-
-  if(document.getElementById("error").innerHTML == ""){
-    var prenom = document.getElementById("prenom").value
-    document.getElementById("resultat").innerHTML = `Bienvenue à ${prenom}`;
-
-    document.getElementById("error").classList.remove("display")
-  document.getElementById("resultat").classList.add("display")
   }
    
 
